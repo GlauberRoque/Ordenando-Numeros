d99aa7700a2f924e3da2adb9df3b9c8acf31b9ac
GlauberRoque <glauberroqueferr@gmail.com>
2021-06-24 21:39:31 -0300

    ordenando1

---
+++ OrdenandoNumeros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OrdenandoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int [] numeros = new int[5];
+
+                for(int i =0; i < numeros.Length; i++)
+                {
+                    Console.WriteLine("Digite um número: ");
+                    numeros[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Array.Sort(numeros);
+                Console.Write("\n\n");
+
+                foreach (int i in numeros)
+                {
+                    Console.WriteLine(i);
+                }
+
+        }
+    }
+}
